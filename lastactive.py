fe6729315e8faed2a075250b3b0b8c4b10ed5bce
Ruben Aguilar <ThatRubenAguilar@gmail.com>
2018-01-18 18:44:44 -0800

    Add threading path for tcpdump monitoring

--- lastactive.py
+++ lastactive.py
@@ -9,7 +9,7 @@ from contextlib import closing
 
 app = Flask(__name__)
 config = Config("lastactive.config")
-last_active_monitor = TcpDumpActivityMonitor(port=config.tcpdump_port(),net_interface=config.tcpdump_network_interface())
+last_active_monitor = TcpDumpActivityMonitor(port=config.tcpdump_port(), net_interface=config.tcpdump_network_interface())
 
 
 @app.route('/last_active_utc', methods=['GET'])
@@ -27,20 +27,20 @@ def get_last_active_time():
     return app.make_response((jsonify(rv), status.HTTP_200_OK))
 
 
-async def launch_flask():
+async def launch_flask_async():
+    launch_flask()
+
+
+def launch_flask():
     app.logger.setLevel(logging.INFO)
     app.run(host='127.0.0.1', port=5002)
 
+
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    with closing(loop):
-        mon_task = asyncio.ensure_future(last_active_monitor.start_monitoring_async(), loop=loop)
-        flask_task = asyncio.ensure_future(launch_flask(), loop=loop)
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            mon_task.cancel()
-            loop.run_until_complete(mon_task)
-            loop.run_until_complete(flask_task)
+    try:
+        last_active_monitor.start_monitoring()
+        launch_flask()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        last_active_monitor.stop_monitoring()
--- tcpdumpactivitymonitor.py
+++ tcpdumpactivitymonitor.py
@@ -4,53 +4,117 @@ import asyncio
 import locale
 from contextlib import closing
 import logging
+import threading
 
 class TcpDumpActivityMonitor():
-    def __init__(self, port=None, net_interface=None):
+    def __init__(self, sudo=False, port=None, net_interface=None):
+        self.__sudo = sudo
         self.__port = port
         self.__net_interface=net_interface
         self.__last_active_time = None
+        self.__tcpdump_proc = None
+        self.__thread = None
+        self.__kill_thread = False
 
-    async def start_monitoring_async(self):
-        self.__last_active_time = time.time()
-        cmd = ["sudo", "tcpdump", "-t"]
+    def __build_tcpdump_command(self):
+        cmd = []
+        if self.__sudo:
+            cmd.append("sudo")
+        cmd.append("tcpdump")
+        cmd.append("-t")
         if self.__net_interface is not None:
             cmd.append("-i")
             cmd.append(self.__net_interface)
         if self.__port is not None:
             cmd.append("port")
             cmd.append(self.__port)
-        tcpdump_proc = await asyncio.create_subprocess_exec(*cmd, stdout=sub.PIPE)
+        return cmd
+
+    async def start_monitoring_async(self):
+        if self.__tcpdump_proc is not None:
+            raise MonitoringInProgress("TcpDump monitoring is already in progress.")
+        self.__last_active_time = time.time()
+        cmd = self.__build_tcpdump_command()
+        self.__tcpdump_proc = await asyncio.create_subprocess_exec(*cmd, stdout=sub.PIPE)
 
         try:
-            async for line in tcpdump_proc.stdout:
+            async for line in self.__tcpdump_proc.stdout:
                 logging.debug(line.decode(locale.getpreferredencoding(False)))
                 self.__last_active_time = time.time()
         except asyncio.CancelledError:
             logging.info("cancelled tcpdump monitoring")
             pass
 
-        if tcpdump_proc.returncode is None:
-            #tcpdump_proc.kill()
-            await tcpdump_proc.wait()
+        if self.__tcpdump_proc.returncode is None:
+            self.__tcpdump_proc.kill()
+            await self.__tcpdump_proc.wait()
         self.__last_active_time = None
+
+    def start_monitoring(self):
+        if self.__tcpdump_proc is not None:
+            raise MonitoringInProgress("TcpDump monitoring is already in progress.")
+        cmd = self.__build_tcpdump_command()
+        self.__tcpdump_proc = sub.Popen(cmd, stdout=sub.PIPE)
+        self.__thread = threading.Thread(target=self.__update_loop)
+        self.__thread.daemon = True
+        self.__thread.start()
+
+    def __update_loop(self):
+        for line in iter(self.__tcpdump_proc.stdout.readline, b''):
+            logging.debug(line.decode(locale.getpreferredencoding(False)))
+            self.__last_active_time = time.time()
+            if self.__kill_thread:
+                break
+
+
+    def stop_monitoring(self):
+        if self.__tcpdump_proc is None:
+            return
+        if self.__tcpdump_proc.returncode is None:
+            self.__tcpdump_proc.kill()
+            self.__tcpdump_proc.wait()
+        self.__kill_thread = True
+        self.__thread.join()
+        self.__tcpdump_proc = None
+        self.__last_active_time = None
+        self.__thread = None
+        self.__kill_thread = False
 
     def last_active_time(self):
         return self.__last_active_time
 
 
-async def loop_call(dump):
+class MonitoringInProgress(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+async def loop_call_async(dump):
     while True:
         print("what")
         print(dump.last_active_time())
         print(time.gmtime(dump.last_active_time()))
         await asyncio.sleep(3)
 
+
+def loop_call(dump):
+    try:
+        while True:
+            print("what")
+            print(dump.last_active_time())
+            print(time.gmtime(dump.last_active_time()))
+            time.sleep(3)
+    except KeyboardInterrupt:
+        pass
+
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
     tcpdump = TcpDumpActivityMonitor(port="443")
-    with closing(loop):
-        asyncio.ensure_future(tcpdump.start_monitoring_async(), loop=loop)
-        asyncio.ensure_future(loop_call(tcpdump), loop=loop)
-        loop.run_forever()
+    # loop = asyncio.get_event_loop()
+    # with closing(loop):
+    #     asyncio.ensure_future(tcpdump.start_monitoring_async(), loop=loop)
+    #     asyncio.ensure_future(loop_call_async(tcpdump), loop=loop)
+    #     loop.run_forever()
+    tcpdump.start_monitoring()
+    loop_call(tcpdump)
+    tcpdump.stop_monitoring()
 
